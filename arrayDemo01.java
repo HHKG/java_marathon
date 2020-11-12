d4bbbdb3090cdd83dc9fdaf6056852a6dc276cad
HuaKang Huang <wb-hhk798061@alibaba-inc.com>
2020-11-12 20:13:18 +0800

    接口冲刺

---
+++ arrayDemo01.java
@@ -0,0 +1,10 @@
+public class arrayDemo01{
+    public static void main(String[] args){
+        int score1=100;
+        int score2=98;
+        int score3=90;
+        System.out.println(score1);
+        System.out.println(score2);
+        System.out.println(score3);
+    }
+}
